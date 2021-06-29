6f276959063e73afa95b11a4eb117069a9bede44
kishan <kishan1994reddy@gmail.com>
2021-06-29 17:36:13 +0530

    added add method file

--- framwork/src/sample_code.java
+++ framwork/src/sample_code.java
@@ -3,5 +3,9 @@ import java.util.Arrays;
 public class sample_code {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        add(5,10);
+    }
+    public static int add(int a, int b){
+        return  a+b;
     }
 }
